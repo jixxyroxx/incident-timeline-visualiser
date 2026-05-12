54dceb7b3b52e11408121fa4e10fda041d03621c
unknown <dvyshnavi58@gmail.com>
2026-05-12 14:43:07 +0530

    Day 10 - Swagger/OpenAPI documentation with JWT security scheme

---
+++ src/main/java/com/internship/tool92/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.internship.tool92.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Incident Timeline Visualiser API")
+                .description("REST API for managing incidents with JWT authentication")
+                .version("1.0.0")
+                .contact(new Contact()
+                    .name("Java Developer 1")
+                    .email("vyshnavi@internship.com")))
+            .addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Authentication"))
+            .components(new Components()
+                .addSecuritySchemes("Bearer Authentication",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
+    }
+}
